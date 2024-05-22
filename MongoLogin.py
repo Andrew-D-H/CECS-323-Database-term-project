ef04566eea76a21bf2a2b86776b5647237bd55c9
Andrew H <49460976+AndrewSKYG@users.noreply.github.com>
2024-05-21 23:47:58 -0700

    Update MongoLogin.py

--- MongoLogin.py
+++ MongoLogin.py
@@ -1,5 +1,5 @@
-password: str = "X9GLjfkFNPLnvLhx"
-username: str = "ahua4874"
-project: str = "CECS-323-Spring-2023"
-hash_name: str = "s9djh4h"
+password: str = "-"
+username: str = "-"
+project: str = "-"
+hash_name: str = "-"
 
